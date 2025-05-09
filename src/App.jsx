5fd49b58bb44eabe1afd215e478302284eab0a22
dhritisxn <dhriti.saxena006@gmail.com>
2025-05-09 22:35:48 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import Hero from "./sections/Hero"
+import ShowcaseSection from "./sections/ShowcaseSection"
+
+import Navbar from "./components/Navbar"
+import LogoShowcase from "./sections/LogoShowcase"
+import FeatureCards from "./sections/FeatureCards"
+
+import TechStack from "./sections/TechStack"
+import Contact from "./sections/Contact"
+import Footer from "./sections/Footer"
+const App = () => {
+  return (
+   <>
+   <Navbar/>
+    <Hero/>
+    <ShowcaseSection/>
+    <LogoShowcase/>
+    <FeatureCards/>
+    <TechStack/>
+    <Contact/>
+    <Footer/>
+   </>
+  )
+}
+
+export default App
